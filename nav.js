d31257d7229c0f01cf57d4fb6c8e17c1d8ad8bb5
cohencoo <cohencoombs@outlook.com>
2023-09-26 21:00:18 +0930

    post assignment cleanup

--- nav.js
+++ nav.js
@@ -2,16 +2,16 @@ const nav = document.createElement("nav")
 nav.innerHTML = `
     <a style="color: white; text-decoration: none;" href="/"><h1>WebMentor</h1></a>
     <div class="links">
-        <a ${location.pathname === "/" ? 'class="active"' : ""} href="/">Home</a>
-        <a ${
-            location.pathname === "/dashboard/" ? 'class="active"' : ""
-        } href="/dashboard">Dashboard</a>
-        <a ${
-            location.pathname.split("/")[2] === "practice" ? 'class="active"' : ""
-        } href="/dashboard/practice/?q=1.1.%20Understanding%20the%20Internet%20and%20the%20World%20Wide%20Web">Practice</a>
-        <a ${
-            location.pathname.split("/")[2] === "flashcards" ? 'class="active"' : ""
-        } href="/dashboard/flashcards/">Flashcards</a>
+        <a class="${location.pathname === "/" ? "active" : ""}" href="/">Home</a>
+        <a class="${
+            location.pathname === "/dashboard/" ? "active" : ""
+        }" href="/dashboard">Dashboard</a>
+        <a class="${
+            location.pathname.split("/")[2] === "practice" ? "active" : ""
+        }" href="/dashboard/practice/?q=1.1.%20Understanding%20the%20Internet%20and%20the%20World%20Wide%20Web">Practice</a>
+        <a class="${
+            location.pathname.split("/")[2] === "flashcards" ? "active" : ""
+        }" href="/dashboard/flashcards/">Flashcards</a>
     </div>
 <div></div>
 `
